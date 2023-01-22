634a59781a188e91bc74f066616344a6981a9dd6
Nurbek Ismoilov <nuronbeck@gmail.com>
2023-01-22 17:51:39 +0500

    SCSS breakpoints mixin usage example

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,21 +1,47 @@
 <template>
   <div :class="$style.page">
     <h1>This is an about page</h1>
+
+    <div :class="$style.cardsContainer">
+      <div :class="$style.card"></div>
+      <div :class="$style.card"></div>
+      <div :class="$style.card"></div>
+      <div :class="$style.card"></div>
+      <div :class="$style.card"></div>
+      <div :class="$style.card"></div>
+      <div :class="$style.card"></div>
+    </div>
   </div>
 </template>
 
 <style module lang="scss">
+.card {
+  width: 100%;
+  min-height: 200px;
+  background-color: $color-primary;
+}
+
+.cardsContainer {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 10px;
+
+  @include breakpoint("md") {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @include breakpoint("lg") {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @include breakpoint("xl") {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
 .page {
   min-height: 100vh;
   background-color: $color-white;
   transition: background-color 0.5s linear;
-
-  @include breakpoint("md") {
-    background-color: $color-grey-light;
-  }
-
-  @include breakpoint("lg") {
-    background-color: $color-grey;
-  }
 }
 </style>
