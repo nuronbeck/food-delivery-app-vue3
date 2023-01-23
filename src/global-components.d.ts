d44e010550db2c16c2efda2f88edb7e301b065f4
webfazliddin <@webfazliddin.com>
2023-01-23 10:37:18 +0500

    component-BaseButton

--- src/global-components.d.ts
+++ src/global-components.d.ts
@@ -1,7 +1,10 @@
 import BaseAlert from "@/components/Base/BaseAlert.vue";
+import BaseButton from "@/components/Base/BaseButton.vue";
+
 
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
     BaseAlert: typeof BaseAlert;
+    BaseButton: typeof BaseButton;
   }
 }
