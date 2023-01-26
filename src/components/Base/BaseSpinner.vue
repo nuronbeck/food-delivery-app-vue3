a7f201d4dab3d3eb44b479364e84da4057c3c988
webfazliddin <@webfazliddin.com>
2023-01-26 19:22:56 +0500

    BaseSpinner component

---
+++ src/components/Base/BaseSpinner.vue
@@ -0,0 +1,67 @@
+<template>
+  <div :class="classNames($style.spinner, $style[variant])"></div>
+</template>
+
+<script setup lang="ts">
+import classNames from "classnames";
+import { defineProps, withDefaults } from "vue";
+
+export interface IBaseSpinner {
+  type: string;
+  variant?: "white" | "primary" | "secondary";
+}
+
+withDefaults(defineProps<IBaseSpinner>(), {
+  type: "",
+  variant: "primary",
+});
+</script>
+<style module lang="scss">
+.spinner {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+
+  &:after {
+    content: " ";
+    display: block;
+    width: 16px;
+    height: 16px;
+    margin: 2px;
+    border-radius: 50%;
+    border: 2px solid #fff;
+    border-color: #fff transparent #fff transparent;
+    animation: lds-dual-ring 1.2s linear infinite;
+  }
+}
+
+.white {
+  &:after {
+    border: 2px solid $color-white !important;
+    border-color: $color-white transparent $color-white transparent !important;
+  }
+}
+
+.primary {
+  &:after {
+    border: 2px solid $color-primary !important;
+    border-color: $color-primary transparent $color-primary transparent !important;
+  }
+}
+
+.secondary {
+  &:after {
+    border: 2px solid $color-secondary !important;
+    border-color: $color-secondary transparent $color-secondary transparent !important;
+  }
+}
+
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/components/global-components.ts
+++ src/components/global-components.ts
@@ -7,14 +7,15 @@ import AppFooter from "@/components/Layout/AppFooter.vue";
 // Base components
 import BaseAlert from "@/components/Base/BaseAlert.vue";
 import BaseButton from "@/components/Base/BaseButton.vue";
-
+import BaseSpinner from "@/components/Base/BaseSpinner.vue";
 
 export default {
   install(app: App): void {
     // Register global components here
     app.component("BaseAlert", BaseAlert);
     app.component("BaseButton", BaseButton);
-    
+    app.component("BaseSpinner", BaseSpinner);
+
     app.component("AppHeader", AppHeader);
     app.component("AppFooter", AppFooter);
   },
