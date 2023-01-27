e2a4d9ca7824018955d10e05e28362db329b87a3
Nurbek Ismoilov <nuronbeck@gmail.com>
2023-01-27 23:06:39 +0500

    Added auth store

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,37 @@
 </template>
 
 <script setup lang="ts">
+import { useAuthStore } from "@/stores/authStore";
+import { ref, onMounted } from "vue";
 import { RouterView } from "vue-router";
+
+const { checkAuth } = useAuthStore();
+const isAppLoading = ref(true);
+
+const setupApplication = () => {
+  if (localStorage.getItem("foodDeliveryAppToken")) {
+    checkAuth();
+    // .then(response => {
+    //   const { data: { user: { id, firstName, lastName, email, phoneNumber } = {} } = {} } = response;
+    //   this.setUserData({
+    //     id,
+    //     firstName,
+    //     lastName,
+    //     email,
+    //     phoneNumber
+    //   })
+    // })
+    // .finally(() => {
+    //    isAppLoading.value = false;
+    // })
+  } else {
+    isAppLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  setupApplication();
+});
 </script>
 
 <style lang="scss">
