ce6c3188c3d8a958bceba195e74fca3b70690542
webfazliddin <@webfazliddin.com>
2023-02-06 14:45:47 +0500

    AuthLayout

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,92 @@
+<template>
+  <div :class="$style.wrapper">
+    <div :class="$style.auth">
+      <RouterLink to="/" :class="$style.authLogo">
+        <img src="@/assets/header/main_logo.svg" />
+      </RouterLink>
+      <div>
+        <RouterView />
+      </div>
+    </div>
+
+    <div :class="$style.authLayout">
+      <div :class="$style.authLayoutTitle">
+        <h2>Strengthen the Guci`an bond</h2>
+      </div>
+      <div :class="$style.authLayoutText">
+        <p>
+          Connecting Alumni and students of Government engineering college
+          Idukki to a next level
+        </p>
+      </div>
+      <div :class="$style.swiperCircle">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style module lang="scss">
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+.auth {
+  position: relative;
+}
+.authLogo {
+  img {
+    margin: 15px 0 0 15px;
+    @include breakpoint("md") {
+      margin: 30px 0 0 30px;
+    }
+  }
+}
+.authLayout {
+  background-color: $color-primary;
+  display: none;
+  flex-direction: column;
+  justify-content: end;
+  text-align: center;
+  height: 100vh;
+  @include breakpoint("md") {
+    display: flex;
+  }
+}
+.authLayoutTitle {
+  font-family: $base-font;
+  font-weight: 700;
+  font-size: 32px;
+  margin-bottom: 16px;
+  line-height: 42px;
+  color: $color-white;
+}
+
+.authLayoutText {
+  font-family: $base-font;
+  font-weight: 400;
+  font-size: 14px;
+  margin-bottom: 40px;
+  line-height: 20px;
+  letter-spacing: 0.1px;
+  color: $color-white;
+}
+
+.swiperCircle {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 60px;
+
+  span {
+    width: 8px;
+    height: 8px;
+    margin: 0 5px;
+    opacity: 0.3;
+    border-radius: 50%;
+    background-color: $color-white;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 import MainLayout from "@/layouts/MainLayout.vue";
+import AuthLayout from "@/layouts/AuthLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,11 @@ const router = createRouter({
           component: () => import("../views/AboutView.vue"),
         },
       ],
+    },
+    {
+      path: "/auth",
+      component: AuthLayout,
+      children: [],
     },
   ],
 });
