5b0cc94c71bc43c86584ac83311032992f6c2409
webfazliddin <@webfazliddin.com>
2023-02-15 11:24:33 +0500

    icons change RouterLink

--- src/components/Layout/AppFooter.vue
+++ src/components/Layout/AppFooter.vue
@@ -3,49 +3,59 @@
     <div class="container">
       <div :class="$style.wrapper">
         <div>
-          <div :class="$style.logo">
-            <RouterLink to="/">
-              <img src="@/assets/footer/logo-white.svg" />
-            </RouterLink>
-          </div>
-          <font-awesome-icon
-            :class="$style.icon"
-            icon="fa-brands fa-facebook-f"
-          />
-          <font-awesome-icon :class="$style.icon" icon="fa-brands fa-twitter" />
-          <font-awesome-icon
-            :class="$style.icon"
-            icon="fa-brands fa-instagram"
-          />
-          <font-awesome-icon :class="$style.icon" icon="fa-brands fa-youtube" />
-          <font-awesome-icon
-            :class="$style.icon"
-            icon="fa-brands fa-linkedin-in"
-          />
+          <RouterLink to="/">
+            <font-awesome-icon
+              :class="$style.icon"
+              icon="fa-brands fa-facebook-f"
+            />
+          </RouterLink>
+          <RouterLink to="/">
+            <font-awesome-icon
+              :class="$style.icon"
+              icon="fa-brands fa-twitter"
+            />
+          </RouterLink>
+          <RouterLink to="/">
+            <font-awesome-icon
+              :class="$style.icon"
+              icon="fa-brands fa-instagram"
+            />
+          </RouterLink>
+          <RouterLink to="/">
+            <font-awesome-icon
+              :class="$style.icon"
+              icon="fa-brands fa-youtube"
+            />
+          </RouterLink>
+          <RouterLink to="/">
+            <font-awesome-icon
+              :class="$style.icon"
+              icon="fa-brands fa-linkedin-in"
+            />
+          </RouterLink>
         </div>
-
         <div>
           <span :class="$style.header">Response</span>
-          <a :class="$style.link">How it works</a>
-          <a :class="$style.link">Guarantee</a>
-          <a :class="$style.link">Security</a>
-          <a :class="$style.link">Pricing</a>
+          <RouterLink to="/" :class="$style.link">How it works</RouterLink>
+          <RouterLink to="/" :class="$style.link">Guarantee</RouterLink>
+          <RouterLink to="/" :class="$style.link">Security</RouterLink>
+          <RouterLink to="/" :class="$style.link">Pricing</RouterLink>
         </div>
 
         <div>
           <span :class="$style.header">Company</span>
-          <a :class="$style.link">About us</a>
-          <a :class="$style.link">Prices</a>
-          <a :class="$style.link">Blog</a>
-          <a :class="$style.link">License</a>
+          <RouterLink to="/" :class="$style.link">About us</RouterLink>
+          <RouterLink to="/" :class="$style.link">Prices</RouterLink>
+          <RouterLink to="/" :class="$style.link">Blog</RouterLink>
+          <RouterLink to="/" :class="$style.link">License</RouterLink>
         </div>
 
         <div>
           <span :class="$style.header">Support</span>
-          <a :class="$style.link">Getting started</a>
-          <a :class="$style.link">Help center</a>
-          <a :class="$style.link">Report a bug</a>
-          <a :class="$style.link">Contact us</a>
+          <RouterLink to="/" :class="$style.link">Getting started</RouterLink>
+          <RouterLink to="/" :class="$style.link">Help center</RouterLink>
+          <RouterLink to="/" :class="$style.link">Report a bug</RouterLink>
+          <RouterLink to="/" :class="$style.link">Contact us</RouterLink>
         </div>
       </div>
     </div>
@@ -106,11 +116,15 @@
 .link {
   color: $color-grey-light;
   font-family: $base-font;
+  text-decoration: none;
   font-weight: 400;
   font-size: 13px;
   line-height: 18px;
   letter-spacing: 0.2px;
   cursor: pointer;
   margin-bottom: 10px;
+  &:hover {
+    color: $color-grey;
+  }
 }
 </style>
